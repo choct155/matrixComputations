2183e3236b8f61cfd147eaf56d4133afb1ff6017
Sarah Kuehl <kuehlse@gmail.com>
2021-08-16 17:39:43 -0400

    working through circuit to matrix code

--- src/python/matrixcomp/ch1_2_17.py
+++ src/python/matrixcomp/ch1_2_17.py
@@ -35,16 +35,15 @@ class CircuitNode:
         current_expressions: List[Add] = CircuitNode.solve_for_current(self.resistor_exprs)
         self.node_expr: Add = CircuitNode.combine_resistor_exprs(current_expressions)
 
-
-    """takes the input circuit node and creates a list of expressions for each current into that node"""
     @staticmethod
     def solve_for_current(resistor_exprs: List[Add]) -> List[Add]:
+        """takes the input circuit node and creates a list of expressions for each current into that node"""
         out: List[Add] = list(map(lambda le: solve(le, I)[0], resistor_exprs))
         return out
 
-    """takes the list of current expressions for a node and combines like variables"""
     @staticmethod
     def combine_resistor_exprs(resistor_exprs: List[Add]) -> Add:
+        """takes the list of current expressions for a node and combines like variables"""
         out: Add = reduce(lambda first, second: first + second, resistor_exprs)
         return out
 
@@ -56,69 +55,61 @@ class CircuitNode:
     # x5: CircuitNode = CircuitNode("x5", [0.5*(e - b) - I, 0.1*(e - d) - I, .5*(e - f) - I, .1*(e - 9) - I], I)
     # x6: CircuitNode = CircuitNode("x6", [0.5*(f - e) - I, 0.1*(f - c) - I, .2(f - 0) - I], I)
     # x7: CircuitNode = CircuitNode("x7", [0.2*(g - d) - I, 0.2*(g - 9) - I], I)
-
     """user input: initial list of nodes"""
-    # x_all: Circuit = [x1, x2, x3, x4, x5, x6, x7]
+    # x_all: Circuit = Circuit([x1, x2, x3, x4, x5, x6, x7])
 
-@dataclass
 class Circuit:
-    #input for initial circuit is a list of CircuitNodes
-    circuit_expr: List[CircuitNode]
-    x_matrix: tf.Tensor
 
-    def __post_init__(self) -> None:
-       empty_dict: Dict[Add, float] = Circuit.create_empty_circuit_dict(self.circuit_expr)
-       a_array: List[List[float]] = Circuit.create_circuit_array(self.circuit_expr, empty_dict)
-       b_list: List[float] = Circuit.create_b_array(self.circuit_expr)
-       a_matrix: tf.Tensor = Circuit.convert_to_tensor(a_array)
-       b_matrix: tf.tensor = Circuit.convert_to_tensor(b_list)
-       self.x_matrix: tf.tensor = solve_for_x(a_matrix, b_matrix)
+    def __init__(self, circuit_expr: List[CircuitNode], x_matrix: tf.Tensor) -> None:
+        self.circuit_expr: List[CircuitNode] = circuit_expr
+        empty_dict: Dict[Symbol, float] = Circuit.create_empty_circuit_dict(self.circuit_expr)
+        full_array: List[List[float]] = Circuit.create_circuit_array(self.circuit_expr, empty_dict)
+        a_array: List[List[float]] = Circuit.create_a_array(full_array)
+        b_list: List[float] = Circuit.create_b_array(full_array)
+        a_matrix: tf.Tensor = Circuit.convert_to_tensor(a_array)
+        b_matrix: tf.tensor = Circuit.convert_to_tensor(b_list)
+        self.x_matrix: tf.Tensor = solve_for_x(a_matrix, b_matrix)
 
-    """creates an empty dict which accounts for all variables"""
     @staticmethod
-    def create_empty_circuit_dict(input_circuit: "Circuit") -> Dict[Add, float]:
-        #determine number of keys needed
-        number_of_keys: int = len(input_circuit)
-        #create alphabetical list of keys
-        key_list: List = []
-        alpha = 'a'
-        for i in range(0, number_of_keys):
-            key_list.append(alpha)
-            alpha = chr(ord(alpha) + 1)
-        #initialize dict to return {a: 0, b: 0, c: 0, d: 0, e: 0, f: 0, g: 0}
-        empty_dict: Dict[Add, float] = dict.fromkeys(key_list, 0.0)
+    def create_empty_circuit_dict(input_circuit: List[CircuitNode]) -> Dict[Symbol, float]:
+        """creates an empty dict which accounts for all variables"""
+        get_symbols: Callable[[CircuitNode], Set[Symbol]] = lambda cn: set(cn.node_expr.as_coefficients_dict().keys())
+        all_symbols: List[Set[Symbol]] = map(get_symbols, input_circuit)
+        symbol_set: Set[Symbol] = reduce(lambda s1, s2: s1.union(s2), all_symbols)
+        #initialize dict to return {a: 0, b: 0, c: 0, d: 0, e: 0, f: 0, g: 0, 1: 0}
+        empty_dict: Dict[Symbol, float] = dict.fromkeys(symbol_set, 0.0)
         return empty_dict
 
-    """takes the expression for a node and creates a list with each node's variables, with 0 as the value for any
-    unaccounted-for keys in the row"""
     @staticmethod
-    def create_circuit_row(input_circuit_node: CircuitNode, empty_dict: Dict[Add, float]) -> Dict[Add, float]:
-        row_dict: Dict[Add, float] = input_circuit_node.node_expr.as_coefficients_dict()
-        updated_row: Dict[Add, float] = empty_dict.update(row_dict)
-        return updated_row
+    def create_circuit_array(input_circuit: List[CircuitNode], empty_dict: Dict[Symbol, float]) -> List[Dict[Symbol, float]]:
+        """maps over the input list of CircuitNodes to create list of row dicts"""
+        #get_row_dict: Callable[[CircuitNode], Dict[Symbol, float]] = lambda cn: empty_dict.update(cn.node_expr.as_coefficients_dict())
+        #output_array_dicts: List[Dict[Symbol, float]] = map(get_row_dict, input_circuit)
+        partial_row_dict: Callable[CircuitNode] = lambda cn: cn.node_expr.as_coefficients_dict()
+        full_row_dict: Callable[Dict[Symbol, float]] = lambda d1: d1.update(empty_dict) or d1
+        partial_array_dicts: List[Dict[Symbol, float]] = map(partial_row_dict, input_circuit)
+        full_array_dicts: List[Dict[Symbol, float]] = map(full_row_dict, partial_array_dicts)
+        #output_array: List[List[float]] = map(lambda x: x.values(), full_array_dicts)
+        return full_array_dicts
 
-    """maps over the input list of CircuitNodes to create an array of just the values in the row dicts"""
     @staticmethod
-    def create_circuit_array(input_circuit: "Circuit", empty_dict: Dict[Add, float]) -> List[List[float]]:
-        output_array: List[List[float]] = map(input_circuit, lambda x: Circuit.create_circuit_row(x, empty_dict).values)
-        return output_array
+    def create_b_array(input_array: List[Dict[Symbol, float]]) -> List[float]:
+        b_array: List[float] = [key[1] for key in input_array]
+        return b_array
 
-    """finds the value in a node expression without a coefficient"""
     @staticmethod
-    def find_non_coefficients(input_node: CircuitNode) -> float:
-        output_number: float = 0.0
-        # want 0 returned if there is no value in the node, but also not sure if this atoms(Number) works the way it says it does
-        # https://www.geeksforgeeks.org/python-sympy-atoms-method/
-        output_number: float = input_node.node_expr.atoms(Number)
-        return output_number
+    def create_a_array(input_array: List[Dict[Symbol, float]]) -> List[List[float]]:
+        a_dict_list: List[Dict[Symbol, float]] = map(lambda x: x.pop(1), input_array)
+        a_array: List[List[float]] = map(lambda a: a.values(), a_dict_list)
+        return a_array
 
-    "creates the b_array with the non-coefficient values"
-    @staticmethod
-    def create_b_array(input_circuit: "Circuit") -> List[float]:
-        out: List[float] = map(input_circuit, lambda x: Circuit.find_non_coefficients(x))
-        return out
+    # @staticmethod
+    # def create_a_b_arrays(input_array: List[List[float]]) -> (List[List[float]], List[float]):
+    #     """separates a and b arrays"""
+    #     b: List[float] = [r.pop(7) for r in input_array]
+    #     return (input_array, b)
 
-    """converts created array to tensor"""
     @staticmethod
     def convert_to_tensor(input) -> tf.Tensor:
+        """converts created array to tensor"""
         return tf.convert_to_tensor(input)
